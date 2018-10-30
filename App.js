bdf13bd34974dcd616790a004cfe59b3dc5d0004
Marcos Barrera del Río <elyomarcos@gmail.com>
2018-10-29 19:09:25 -0600

    (#3) - Add Drawer Navigator for the AppStack Routing Component

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { createStackNavigator, createSwitchNavigator } from 'react-navigation';
+import React from 'react';
+import { Text } from 'react-native';
+
+import { createStackNavigator, createSwitchNavigator, createDrawerNavigator } from 'react-navigation';
 
 import SignInScreen from './components/Screens/SignInScreen/SignInScreen';
 import AuthLoadingScreen from './components/Screens/AuthLoadingScreen/AuthLoadingScreen';
 import DietScreen from './components/Screens/DietScreen/DietScreen';
 import DashboardScreen from './components/Screens/DashboardScreen/DashboardScreen';
 
-const AppStack = createStackNavigator({ Dashboard: DashboardScreen, Diet: DietScreen });
+const AppStack = createDrawerNavigator({ 
+  Dashboard: createStackNavigator({ Dashboard: DashboardScreen }),
+  Dietas: createStackNavigator({ Diet: DietScreen }),
+});
 const AuthStack = createStackNavigator({ SignIn: SignInScreen });
 
 const RootSwitchStack = createSwitchNavigator(
