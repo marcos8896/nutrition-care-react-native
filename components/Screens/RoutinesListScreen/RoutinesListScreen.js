9d042abc55ddfcaaa331f4230756f0cc15b1e55a
Brandon Villa <bornofos@gmail.com>
2018-11-01 20:01:52 -0600

    RoutineList Screen
    Component to render an ScrollView in order to display all routines

--- components/Screens/RoutinesListScreen/RoutinesListScreen.js
+++ components/Screens/RoutinesListScreen/RoutinesListScreen.js
@@ -1,19 +1,47 @@
 import React from 'react';
-import styles from '../../../globalStyles';
 
-import { StatusBar, View, Text } from 'react-native';
+import {
+  ScrollView,
+} from 'react-native';
+import ListItem from '../../shared/ListItem/ListItem';
+import { getRoutines } from '../../../api/routines';
 
 class RoutinesListScreen extends React.Component {
   static navigationOptions = {
     title: 'Rutinas',
   };
+  constructor(props) {
+    super(props);
+    this.state = {
+      routines: [],
+      error: null
+    }
+    this.renderItems = this.renderItems.bind(this);
+  }
+  componentDidMount() {
+    getRoutines()
+      .then(response => this.setState({ routines: response }))
+      .catch(error => {
+        Alert.alert(
+          'Error',
+          'Ha ocurrido un error inesperado :( \n' + error.toString(),
+          [{ text: 'OK' }],
+          { cancelable: false }
+        );
+      })
+  }
+
+  renderItems() {
+    return this.state.routines.map(
+      item => <ListItem key={item.id} title={item.description}/>
+    )
+  }
 
   render() {
     return (
-      <View style={styles.container}>
-        <StatusBar barStyle="default" />
-        <Text>Routines list</Text>
-      </View>
+      <ScrollView style={{backgroundColor: '#FFF'}}>
+        {this.renderItems()}
+      </ScrollView>
     );
   }
 };
