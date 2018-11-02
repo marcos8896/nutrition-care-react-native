1cd22568afe5b2e2f99ccafa4452daadbd08aa66
Marcos Barrera del Río <elyomarcos@gmail.com>
2018-11-01 20:36:04 -0600

    (#11) - Add List to display all the diets from the current user

--- components/Screens/DietScreen/DietScreen.js
+++ components/Screens/DietScreen/DietScreen.js
@@ -1,21 +1,122 @@
 import React from 'react';
-import styles from '../../../globalStyles';
+import { 
+  View, 
+  Text,
+  AsyncStorage,
+  FlatList,
+  Alert,
+  StyleSheet,
+} from 'react-native';
 
-import { StatusBar, View, Text } from 'react-native';
+import { BASE_API_URL } from '../../../constants';
 
-class DietScreen extends React.Component {
+class DietScreen extends React.PureComponent {
   static navigationOptions = {
     title: 'Dietas',
   };
 
+  state = {
+    diets: []
+  }
+
+  componentDidMount() {
+    this.getDiets().then( diets => {
+
+      diets.forEach( diet => {
+        diet.key = `${diet.id}`
+        diet.date = new Date(diet.createdAt).toLocaleDateString();
+      });
+      
+      this.setState({ diets: diets });
+      
+    })
+    .catch( err => {
+      Alert.alert(
+        'Error',
+        'Hubo un error al tratar de obtener las dietas :(',
+        [{ text: 'OK' }],
+        { cancelable: false }
+      );
+    });
+  }
+
+  getDiets = async () => {
+
+    const [ userToken, userId ] = await Promise.all([
+      AsyncStorage.getItem('@app:userToken'),
+      AsyncStorage.getItem('@app:userId'),
+    ]);
+
+    return fetch(`${BASE_API_URL}/Customers/${userId}/diets`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': userToken,
+      },
+    })
+    .then(response => response.json())
+  }
+
+  renderItem = ({ item }) => {
+    return (
+      <View style={styles.row}>
+
+        <Text style={styles.description}>
+          {item.description}
+        </Text>
+
+        <View style={styles.infoContainer}>
+
+          <View style={styles.nutrients}>
+            <Text> Calorías: {item.totalCarbohydrates} </Text>
+            <Text> Proteínas: {item.totalProteins} </Text>
+            <Text> Grasas: {item.totalFats} </Text>
+            <Text> Calorias: {item.totalCalories} </Text>
+          </View>
+
+          <View>
+            <Text> Fecha:</Text>
+            <Text> {item.date} </Text>
+          </View>
+
+        </View>
+      </View>
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <StatusBar barStyle="default" />
-        <Text>Diet content</Text>
+        <FlatList
+          data={this.state.diets}
+          renderItem={this.renderItem}
+        />
       </View>
     );
   }
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  row: {
+    padding: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee'
+  },
+  nutrients: {
+    flex: 1
+  },
+  description: {
+    fontSize: 18
+  },
+  infoContainer: {
+    flex: 1,
+    flexDirection: 'row'
+  }
+});
 
 export default DietScreen;
