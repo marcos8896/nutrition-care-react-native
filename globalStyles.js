82745499c4d0d0e2ef0c36caef574f83692558d8
Brandon Villa <bornofos@gmail.com>
2018-11-06 19:35:56 -0600

    Add global styles

--- globalStyles.js
+++ globalStyles.js
@@ -4,8 +4,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#FFF'
+    justifyContent: 'flex-start',
   },
 });
 
