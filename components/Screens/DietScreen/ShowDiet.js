beb0414987d6b753e0986827131c1b773920548e
Marcos Barrera del Río <elyomarcos@gmail.com>
2018-11-07 04:55:24 -0600

    (#11) - Get the full diet record with relations from Loobpack

--- components/Screens/DietScreen/ShowDiet.js
+++ components/Screens/DietScreen/ShowDiet.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import axios from 'axios';
 
 import { 
   View,
   Text,
   AsyncStorage,
   StyleSheet,
+  Alert,
 } from 'react-native';
 
 import { BASE_API_URL } from '../../../constants';
@@ -17,25 +19,53 @@ class ShowDiet extends React.PureComponent {
   }
 
   state = {
-    diet: null
+    diet: null,
+    json: 'null'
   }
 
-  componentDidMount() {
+  async componentDidMount() {
+
+    const dietId = this.props.navigation.getParam('dietId', '');
+    const diet = await this.getFullDiet( dietId );
+    this.setState({ json: JSON.stringify(diet, null, '  ') })
+
   }
 
-  getFullDiet = async () => {
+  getFullDiet = async ( dietId ) => {
 
     const [ userToken, userId ] = await Promise.all([
       AsyncStorage.getItem('@app:userToken'),
       AsyncStorage.getItem('@app:userId'),
     ]);
+    
+
+    const filter = {
+      where: { id: dietId },
+      include: ['dietFoodDetails'],
+    };
+
+    return axios.get(`${BASE_API_URL}/Customers/${userId}/diets/`, { 
+      params: { filter: filter },
+      headers: { 'Authorization': userToken }
+    })
+    .then( response => response.data )
+    .then( ( [diet] ) => diet )
+    .catch( err => {
+      console.log('err: ', err);
+      Alert.alert(
+        'Error',
+        'Hubo un error al tratar de obtener la dieta completa :(',
+        [{ text: 'OK' }],
+        { cancelable: false }
+      );
+    });
 
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text>Show diet content</Text>
+        <Text>Cambia id: { this.state.json }</Text>
       </View>
     );
   }
@@ -43,24 +73,11 @@ class ShowDiet extends React.PureComponent {
 
 const styles = StyleSheet.create({
   container: {
+    paddingHorizontal: 15,
+    paddingVertical: 15,
     flex: 1,
     backgroundColor: '#fff'
   },
-  row: {
-    padding: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: '#eee'
-  },
-  nutrients: {
-    flex: 1
-  },
-  description: {
-    fontSize: 18
-  },
-  infoContainer: {
-    flex: 1,
-    flexDirection: 'row'
-  }
 });
 
 export default ShowDiet;
