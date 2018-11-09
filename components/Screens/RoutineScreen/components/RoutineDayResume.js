2aad00c87f515877b7904d12fe40a39a5583932e
Brandon Villa <bornofos@gmail.com>
2018-11-08 19:36:04 -0600

    Component for display a daily routine details

---
+++ components/Screens/RoutineScreen/components/RoutineDayResume.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View } from 'react-native';
+import { IMAGES_API_URL } from '../../../../constants';
+import CardExercise from '../../../shared/CardExercise/CardExercise';
+import Paper from '../../../shared/Paper/Paper';
+
+
+const RoutineDayResume = ({ name, exercises }) => {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Paper text={name.toUpperCase()} />
+      </View>
+      {
+        exercises.map((exerciseData, index) => {
+          const { description, reps, series } = exerciseData;
+          const { name, imageName } = exerciseData.exercise;
+          return (
+            <CardExercise
+              key={index}
+              image={`${IMAGES_API_URL}/${imageName}`}
+              header={name}
+              body={description}
+              series={series}
+              reps={reps}
+            />
+          )
+        })
+      }
+    </View>
+  )
+}
+
+export default RoutineDayResume;
