b3e330089527cfb9d9e4da4ea3fdb726c2b433c4
Brandon Villa <bornofos@gmail.com>
2018-11-08 19:35:47 -0600

    Routine Screen save in state and display routine

--- components/Screens/RoutineScreen/RoutineScreen.js
+++ components/Screens/RoutineScreen/RoutineScreen.js
@@ -1,29 +1,58 @@
 import React from 'react';
 import styles from '../../../globalStyles';
 
-import { StatusBar, View, Text } from 'react-native';
+import { ScrollView, Text } from 'react-native';
 import { getRoutine } from '../../../api/routines';
+import RoutineDayResume from './components/RoutineDayResume';
 
 class RoutineScreen extends React.Component {
   static navigationOptions = {
     title: 'Rutina',
   };
   state = {
+    description: '',
     routine: {}
   }
   componentDidMount() {
     const { navigation } = this.props
     const routineId = navigation.getParam('routineId')
     getRoutine( routineId )
-      .then(routine => this.setState({ routine }))
-      .catch(err => console.log('Agh', err))
+      .then(data => this.setState({
+        description: data.description,
+        routine: this.mapRoutine( data.exerciseRoutineDetails )
+      }))
+      .catch(err => { throw err; })
   }
+  
+  mapRoutine = exerciseRoutineDetails => {
+    return exerciseRoutineDetails.reduce( ( routine, item ) => {
+      const prevExercises = routine[item.day] || {}; // To avoid spread over undefined
+      return {
+        ...routine,
+        [item.day]: [
+          ...prevExercises,
+          item
+        ]
+      };
+    }, {});
+  }
+
   render() {
+    const { routine } = this.state;
     return (
-      <View style={styles.container}>
-        <StatusBar barStyle="default" />
-        <Text>{JSON.stringify(this.state.routine)}</Text>
-      </View>
+      <ScrollView>
+        {
+          Object.keys(routine).map((key, index) => {
+            return (
+              <RoutineDayResume
+                key={index}
+                name={key}
+                exercises={routine[key]}
+              />
+            );
+          })
+        }
+      </ScrollView>
     );
   }
 };
