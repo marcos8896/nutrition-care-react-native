bdbddef0041125b6f61e111d961af371883cde98
Marcos Barrera del Río <elyomarcos@gmail.com>
2018-11-12 21:18:19 -0600

    Add foodDetails List to the ShowDiet component

--- components/Screens/DietScreen/ShowDiet/ShowDiet.js
+++ components/Screens/DietScreen/ShowDiet/ShowDiet.js
@@ -4,7 +4,15 @@ import axios from 'axios';
 import DietGeneralInfo from './components/DietGeneralInfo';
 import Divider from '../../../shared/Divider/Divider';
 
-import { View, AsyncStorage, StyleSheet, Alert, Text } from 'react-native';
+import { 
+ View, 
+ AsyncStorage, 
+ StyleSheet, 
+ Alert, 
+ Text,
+ FlatList,
+ TouchableOpacity,
+} from 'react-native';
 
 import { BASE_API_URL } from '../../../../constants';
 
@@ -18,7 +26,6 @@ class ShowDiet extends React.PureComponent {
   state = {
     diet: {},
     dietFoodDetails: [],
-    json: 'null'
   }
 
   async componentDidMount() {
@@ -29,7 +36,7 @@ class ShowDiet extends React.PureComponent {
 
     this.setState({
       diet,
-      json: JSON.stringify(diet, null, '  ') 
+      dietFoodDetails: diet.dietFoodDetails,
     })
 
   }
@@ -52,7 +59,10 @@ class ShowDiet extends React.PureComponent {
       headers: { 'Authorization': userToken }
     })
     .then( response => response.data )
-    .then( ( [diet] ) => diet )
+    .then( ( [diet] ) => {
+      diet.dietFoodDetails = this.calculateNutrientValues(diet.dietFoodDetails);
+      return diet;
+    })
     .catch( err => {
       console.log('err: ', err);
       Alert.alert(
@@ -65,6 +75,67 @@ class ShowDiet extends React.PureComponent {
 
   }
 
+/**
+ * 
+ * Rounded decimals from float numbers and only allows them to have two decimals.
+ * @author Marcos Barrera del Río <elyomarcos@gmail.com>
+ * @param num - Number to round.
+ * @returns number - Rounded number.
+ */
+  roundNumber = ( num ) => Math.round( num * 100 ) / 100;
+
+  calculateNutrientValues = ( dietFoodDetails ) => {
+    dietFoodDetails.forEach( (food, index) => {
+      
+      //Calculate food nutrients.
+      food.calculatedCalories = this.roundNumber(food.calories * food.desiredGrams);
+      food.calculatedProteins = this.roundNumber(food.proteins * food.desiredGrams);
+      food.calculatedFats = this.roundNumber(food.fats * food.desiredGrams);
+      food.calculatedCarbs = this.roundNumber(food.carbohydrates * food.desiredGrams);
+
+      //Set key for the List uniquess
+      food.key = `${food.dietId}-${food.foodId}-${index}`;
+
+    });
+
+    return dietFoodDetails;
+  }
+
+  renderDetailItem = ({ item, index }) => {
+
+    let rowStyles;
+
+    if(index % 2 !== 0)
+      rowStyles = [styles.row, styles.oddRow];
+    else
+      rowStyles = styles.row;
+
+    return (
+      <TouchableOpacity 
+        style={rowStyles}
+      >
+
+        <Text style={styles.description}>
+          {item.description}:
+        </Text>
+
+        <View>
+
+          <Text> 
+            Por cada {item.desiredGrams} gramos, incluye:
+          </Text>
+
+          <Text> Calorías - {item.calculatedCalories} </Text>
+          <Text> Proteínas - {item.calculatedProteins} </Text>
+          <Text> Grasas - {item.calculatedFats} </Text>
+          <Text> Carbohidratos - {item.calculatedCarbs} </Text>
+          <Text> Key - {item.key} </Text>
+
+        </View>
+      </TouchableOpacity>
+    );
+  }
+
   render() {
     return (
       <View style={styles.paper}>
@@ -72,9 +143,12 @@ class ShowDiet extends React.PureComponent {
         <Divider/>
         <Text>Creado el: {this.state.diet.date}</Text>
         <Divider/>
-
-        {/* <Text>Cambia id: { this.state.json }</Text> */}
-        
+        <View style={styles.listContainer}>
+          <FlatList
+            data={this.state.dietFoodDetails}
+            renderItem={this.renderDetailItem}
+          />
+        </View>
       </View>
     );
   }
@@ -82,6 +156,7 @@ class ShowDiet extends React.PureComponent {
 
 const styles = StyleSheet.create({
   paper: {
+    flex: 1,
     backgroundColor: '#FFF',
     borderColor: '#DCDCDC',
     borderBottomWidth: 2,
@@ -91,6 +166,23 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     borderRadius: 5,
     padding: 8,
+  },
+  row: {
+    borderBottomWidth: 1,
+    borderLeftWidth: 1,
+    borderTopWidth: 1,
+    borderRightWidth: 1,
+    borderColor: '#eee',
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    borderRadius: 5,
+  },
+  oddRow: {
+    backgroundColor: '#fcfcfc',
+  },
+  listContainer: { 
+    alignSelf: 'stretch',
+    marginBottom: 150,
   }
 });
 
